d810ccf85d936ecf7a5278b8c36c6681ac688ad2
verybada <lon_bad@hotmail.com>
2016-04-02 17:53:38 +0800

    Refactory rarbg.py

--- rarbg.py
+++ rarbg.py
@@ -8,6 +8,7 @@ import sys
 import json
 import os
 import datetime
+import urlparse
 
 import requests
 import lxml
@@ -20,6 +21,24 @@ from .filter import Filter
 
 
 LOG = logging.getLogger()
+
+
+class Rarbg(object):
+    def __init__(self):
+        # TODO: how to pass bot check
+        self._cookie = 'c_cookie=ljd3gcszby; expla2=1%7CSat%2C%2005%20Mar%202016%2021%3A46%3A26%20GMT; LastVisit=1457192974; vDVPaqSe=r9jSB2Wk; expla=2; tcc; __utma=9515318.355702320.1444490883.1457101385.1457192786.76; __utmb=9515318.4.10.1457192786; __utmc=9515318; __utmz=9515318.1447162163.12.4.utmcsr=google|utmccn=(organic)|utmcmd=organic|utmctr=(not%20provided)'
+        self._host = 'http://rarbg.to'
+
+    def conn(self, uri, query=None):
+        url = urlparse.urljoin(self._host, uri)
+        header = {
+            'Cookie': self._cookie
+        }
+
+        sess = requests.Session()
+        resp = sess.get(url, params=query, headers=header)
+        resp.raise_for_status()
+        return resp
 
 
 class TorrentListPage(object):
@@ -43,11 +62,11 @@ class TorrentListPage(object):
         _, info_tag, date_tag, size_tag, _, _, _, _ = tr.find_all('td')
         imdb_tag = info_tag.find('span', style=re.compile('color*'))
         result['screen_size'] = "UNKNOWN"
-        result['href'] = ''.join((self._host, info_tag.a['href']))
+        result['href'] = urlparse.urljoin(self._host, info_tag.a['href'])
         result['imdb'] = self._get_imdb(imdb_tag)
         result['size'] = size_tag.text
         result.update(guessit(info_tag.a['title']))
-        return result
+        return RarbgTorrent(result)
 
     def __iter__(self):
         lista2t_table = self._soup.find('table', class_='lista2t')
@@ -56,13 +75,52 @@ class TorrentListPage(object):
             yield self._parse(tr)
 
 
-class RarbgPager(object):
+class RarbgTorrent(Rarbg, dict):
+    def __init__(self, raw):
+        super(RarbgTorrent, self).__init__()
+        self._raw = raw
+        self._page_href = raw['href']
+        self._page_soup = None
+
+        self.title = raw['title']
+        self.year = raw.get('year', 'UNKNOWN')
+        self.resolution = raw['screen_size']
+        self.format = raw.get('format', 'UNKNOWN')
+        self.size = raw['size']
+        self.video_codec = raw.get('video_codec')
+        self.audio_codec = raw.get('audio_codec')
+        self.imdb = raw['imdb']
+
+    def __str__(self):
+        return "%s.%s.%s.%s.%s.%s.%s" % (self.title, self.year,
+                                         self.resolution, self.format,
+                                         self.video_codec, self.audio_codec,
+                                         self.size)
+
+    @property
+    def href(self):
+        if not self._page_soup:
+            resp = self.conn(self._page_href)
+            self._page_soup = BeautifulSoup(resp.content, 'lxml')
+
+        a = self._page_soup.find('a', href=re.compile('download.php'))
+        return urlparse.urljoin(self._host, a["href"])
+
+    @property
+    def cover(self):
+        if not self._page_soup:
+            resp = self.conn(self._page_href)
+            self._soup = BeautifulSoup(resp.content, 'lxml')
+
+        img = self._page_soup.find("img", itemprop="image")
+        return urlparse.urljoin(self._host, img['src'])
+
+
+class RarbgPager(Rarbg):
     def __init__(self, category=None, search=None,
                  start_page=1, end_page=None):
-        # TODO: how to pass bot check
-        self._cookie = 'c_cookie=ljd3gcszby; expla2=1%7CSat%2C%2005%20Mar%202016%2021%3A46%3A26%20GMT; LastVisit=1457192974; vDVPaqSe=r9jSB2Wk; expla=2; tcc; __utma=9515318.355702320.1444490883.1457101385.1457192786.76; __utmb=9515318.4.10.1457192786; __utmc=9515318; __utmz=9515318.1447162163.12.4.utmcsr=google|utmccn=(organic)|utmcmd=organic|utmctr=(not%20provided)'
-        self._host = 'http://rarbg.to'
-        self._url = '/'.join(x for x in (self._host, 'torrents.php'))
+        super(RarbgPager, self).__init__()
+        self._uri = "torrents.php"
         self._category = category
         self._search = search
         self._current_index = start_page
@@ -74,9 +132,6 @@ class RarbgPager(object):
         return self._page
 
     def _query(self):
-        header = {
-            'Cookie': self._cookie
-        }
         query = {
             'page': self._current_index
         }
@@ -86,10 +141,7 @@ class RarbgPager(object):
         if self._search:
             query['search'] = self._search
 
-        sess = requests.Session()
-        resp = sess.get(self._url, params=query, headers=header)
-        resp.raise_for_status()
-        return resp
+        return self.conn(self._uri, query=query)
 
     def _get_next_index(self, resp):
         soup = BeautifulSoup(resp.content, 'lxml')
@@ -133,7 +185,7 @@ class RARBGspider(object):
         self._db_conn = self._get_db_connection()
         self._pool = MoviePool(self._db_conn)
         # FIXME: category 44 = 1080p movie
-        self._pager = RarbgPager(category=44)
+        self._pager = RarbgPager(category=44, search="2016")
         self._setting_logger()
 
     def _setting_logger(self):
@@ -207,7 +259,8 @@ class RARBGspider(object):
     def _convert_to_movie_info(self, torrent):
         info = None
         try:
-            info = MovieInfo(torrent)
+            info = MovieInfo(torrent._raw)
+            info.href = torrent.href
         except Exception as exp:
             LOG.warn("Incomplete torrent %s", torrent)
         return info
@@ -219,17 +272,18 @@ class RARBGspider(object):
                 break
 
             for torrent in page:
+                if torrent is None:
+                    continue
+
+                if not self._filter.filter(torrent):
+                    LOG.debug("Skip %s", torrent)
+                    continue
+
                 info = self._convert_to_movie_info(torrent)
-                if info is None:
-                    continue
-
-                if not self._filter.filter(info):
-                    LOG.debug("Skip %s", info)
-                    continue
-
                 if self._pool.find(info.href) is not None:
                     # FIXME: ugly
                     stop = True
+                    LOG.info("duplication torrent, stop")
                     break
 
                 LOG.info("New torrent %s", info)
@@ -237,15 +291,19 @@ class RARBGspider(object):
                 self._handlers.register(info)
 
         self._handlers.submit()
+        LOG.info("Crawl done")
 
 
 if __name__ == "__main__":
     assert len(sys.argv) == 2
 
-    conf_path = sys.argv[1]
-    with open(conf_path, "rb") as fp:
-        conf = fp.read()
-        conf_dict = json.loads(conf)
-        r = RARBGspider(conf_dict)
-        r.crawl()
-        r.close()
+    try:
+        conf_path = sys.argv[1]
+        with open(conf_path, "rb") as fp:
+            conf = fp.read()
+            conf_dict = json.loads(conf)
+            r = RARBGspider(conf_dict)
+            r.crawl()
+            r.close()
+    except Exception as exp:
+        LOG.exception(exp)
