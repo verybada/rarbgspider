588c8b6303f3a5f7318f52ccca7d501f4f438d34
verybada <lon_bad@hotmail.com>
2017-06-04 17:11:06 +0800

    rarbgdaemon: setup loggers

--- rarbgsubscriber/rarbgdaemon.py
+++ rarbgsubscriber/rarbgdaemon.py
@@ -1,6 +1,7 @@
 import sys
 import json
 import time
+import logging
 import sqlite3
 import argparse
 
@@ -30,11 +31,54 @@ class RarbgDaemon(Daemon):
         subscriber.join()
 
 
+def setup_logger(workspace, verbose=False):
+    log_dict = {
+        "version": 1,
+        "disable_existing_loggers": False,
+        "propagate": True,
+        "loggers": {
+            '': {
+                "level": logging.DEBUG if verbose else logging.INFO,
+                "handlers": ["console", "file"]
+            },
+        },
+        "handlers": {
+            "console": {
+                "class": "logging.StreamHandler",
+                "level": "DEBUG",
+                "formatter": "precise",
+            },
+            "file": {
+                "class": "logging.handlers.TimedRotatingFileHandler",
+                "level": "DEBUG",
+                "filename": "%s/rarbgsubscriber.log" % workspace,
+                "formatter": "detail",
+                "when": "D",
+                "backupCount": 7
+            }
+        },
+        "formatters": {
+            "precise": {
+                "format": "%(asctime)s - %(message)s"
+            },
+            "detail": {
+                "format": ("%(asctime)s %(filename)s:%(funcName)s:"
+                           "%(lineno)d - %(message)s")
+            }
+        },
+    }
+
+    logging.config.dictConfig(log_dict)
+
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--conf', required=True)
+    parser.add_argument('--log', required=True)
     parser.add_argument('--db', required=True)
+    parser.add_argument('-v', '--verbose', action='store_true')
     args = parser.parse_args()
+    setup_logger(args.log, args.verbose)
     daemon = RarbgDaemon(args.conf, args.db)
     daemon.start()
 
